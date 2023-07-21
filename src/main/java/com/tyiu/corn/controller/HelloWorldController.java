98bc72866b5e2c922e82deac40ebf8a18f115e51
lexunok <lex48949@gmail.com>
2023-07-21 09:06:36 +0500

    first init

---
+++ src/main/java/com/tyiu/corn/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.tyiu.corn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloWorldController {
+
+    @GetMapping("hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
---
+++ src/test/java/com/tyiu/corn/CornApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyiu.corn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
