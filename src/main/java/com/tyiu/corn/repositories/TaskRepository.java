21dbb183aae1c37082899b1188906980d8d014e0
wtckk <nikita.shutnikov89@gmail.com>
2023-07-26 16:22:09 +0500

    fix problems

--- src/main/java/com/tyiu/corn/repositories/TaskRepository.java
+++ src/main/java/com/tyiu/corn/repositories/TaskRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByTitle(String title);
+    List<Task>findByTitle(String title);
 
 }
--- src/main/java/com/tyiu/corn/service/TaskService.java
+++ src/main/java/com/tyiu/corn/service/TaskService.java
@@ -1,31 +1,34 @@
 package com.tyiu.corn.service;
 
-
+import com.tyiu.corn.model.Task
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.tyiu.corn.repositories.TaskRepository;
+
+import java.util.List;
 
 @Service
 class TaskService {
 
 
     private final TaskRepository taskRepository;
+
     @Autowired
     TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
-//    public List<Task> listTask(String title) {
-//        if (title != null)  taskRepository.findByTitle(title);
-//        return taskRepository.findAll();
-//    }
+    public List<Task> listTask(String title) {
+        if (title != null)  taskRepository.findByTitle(title);
+        return taskRepository.findAll();
+    }
 
-//    public void saveTask(Task task) {
-//        TaskRepository.save(task);
-//    }
-//
-//    public void deleteTask(Long id) {
-//        TaskRepository.deleteById(id);
-//    }
-//
+    public void saveTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
 }
