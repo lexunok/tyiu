e253f05eebdec984f186de6b061153ce5e6c05c9
wtckk <nikita.shutnikov89@gmail.com>
2023-07-26 14:15:56 +0500

    Controller

--- src/main/java/model/Task.java
+++ src/main/java/model/Task.java
@@ -3,74 +3,21 @@ package model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @Entity
-public class Task {
+@Data
+@AllArgsConstructor
+public class Task{
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
     private String assignedTo;
     private int priority; // 1-маленький приоритет, 2 - средний, 3 - высокий
     private String deadline;
-
-    public Task(Long id, String title, String description, String assignedTo, int priority, String deadline) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.assignedTo = assignedTo;
-        this.priority = priority;
-        this.deadline = deadline;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getAssignedTo() {
-        return assignedTo;
-    }
-
-    public int getPriority() {
-        return priority;
-    }
-
-    public String getDeadline() {
-        return deadline;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setAssignedTo(String assignedTo) {
-        this.assignedTo = assignedTo;
-    }
-
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public void setDeadline(String deadline) {
-        this.deadline = deadline;
-    }
 }
