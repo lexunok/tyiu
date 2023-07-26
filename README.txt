57f1465d84836d2f11396115961fe62398140308
lexunok <lex48949@gmail.com>
2023-07-26 11:06:51 +0500

    docker compose

---
+++ README.txt
@@ -0,0 +1,5 @@
+В первую очередь запуск контейнеров Docker в cmd
+docker compose up -d
+
+Далее запускайте CornApplication
+/src/main/java/com/tyiu/corn
--- src/main/java/com/tyiu/corn/controller/HelloWorldController.java
+++ src/main/java/com/tyiu/corn/controller/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class HelloWorldController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String sayHello(){
         return "Hello World!";
     }
