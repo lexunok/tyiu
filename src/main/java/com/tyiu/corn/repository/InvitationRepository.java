750c31ae5e5c9417a89900574190a640682a8773
TimurMA <timur.minyazeff@gmail.com>
2023-08-01 12:58:13 +0500

    invitation repository fix

--- src/main/java/com/tyiu/corn/repository/InvitationRepository.java
+++ src/main/java/com/tyiu/corn/repository/InvitationRepository.java
@@ -4,11 +4,10 @@ import com.tyiu.corn.model.Invitation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Date;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Long>{
     @Query("DELETE i FROM Invitation i WHERE i.expiredAt > :date")
-    public List<Invitation> deleteExpiredInvitations(Date date);
+    public void deleteExpiredInvitations(Date date);
 
 }
