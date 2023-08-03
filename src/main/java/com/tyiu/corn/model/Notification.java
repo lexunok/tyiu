47285601216e9d1fa817d126c5bf024a8893d63f
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-08-03 18:37:56 +0500

    Add files via upload

---
+++ src/main/java/com/tyiu/corn/model/Notification.java
@@ -0,0 +1,25 @@
+package com.tyiu.corn.model;
+
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification {
+    @Id
+    @GeneratedValue
+    private Long id;    
+    private String message;
+    private Date date;
+    private int reciever;
+
+}
