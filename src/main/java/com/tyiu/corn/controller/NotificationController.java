f1720adc963a2a4decf99cf2a57c5691fa67efc3
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-08-03 18:37:27 +0500

    Add files via upload

---
+++ src/main/java/com/tyiu/corn/controller/NotificationController.java
@@ -0,0 +1,40 @@
+package com.tyiu.corn.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tyiu.corn.model.Notification;
+import com.tyiu.corn.service.NotificationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/notification")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    public NotificationService getNotificationService() {
+        return notificationService;
+    }
+
+    @PostMapping("/notification")
+    public ResponseEntity<Notification> createNotification(@RequestBody Notification notification) {
+        Notification createdNotification = notificationService.save(notification);
+        return createNotification(createdNotification);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+    
