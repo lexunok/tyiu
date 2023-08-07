d572684808f9c8fe3f7a9f47ba2191e0a824dbe4
TimurMA <timur.minyazeff@gmail.com>
2023-08-07 20:09:11 +0500

    change invitation service

--- src/main/java/com/tyiu/corn/service/InvitationService.java
+++ src/main/java/com/tyiu/corn/service/InvitationService.java
@@ -3,26 +3,17 @@ package com.tyiu.corn.service;
 import com.tyiu.corn.exception.NotFoundException;
 import com.tyiu.corn.model.dto.InvitationDTO;
 import com.tyiu.corn.exception.EmailSendException;
-import com.tyiu.corn.exception.FileReadException;
 import com.tyiu.corn.model.entities.Invitation;
-import com.tyiu.corn.model.enums.Role;
 import com.tyiu.corn.repository.InvitationRepository;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSendException;
@@ -45,15 +36,12 @@ public class InvitationService {
         this.emailSender.send(simpleMailMessage);
     }
 
-    private void sendInvitations(List<String> emails, List<Role> roles) throws MailSendException, NotFoundException {
+    public void sendInvitations(InvitationDTO invitations) throws MailSendException, NotFoundException {
         try {
-            Date date = new Date();
-            long milliseconds = date.getTime() + 259200000;
-            date.setTime(milliseconds);
-            emails.forEach((email) ->
+            invitations.getEmails().stream().forEach((email) ->
                 {
                     Invitation invitation = new Invitation();
-                    invitation.setRoles(roles);
+                    invitation.setRoles(invitations.getRoles());
                     invitation.setEmail(email);
                     sendInvitation(invitation);
                 }
@@ -61,39 +49,8 @@ public class InvitationService {
         } catch ( MailSendException e){
             throw new EmailSendException("В списке есть почта в неправильном формате");
         } catch ( NullPointerException e){
-            throw new NotFoundException("Добавьте почту");
+            throw new NotFoundException("Добавьте фаил с почтами");
         }
-    }
-
-    private List<String> findEmails(String text) throws NotFoundException{
-        Pattern p = Pattern.compile("([\\w\\-]([\\.\\w])+[\\w]+@([\\w\\-]+\\.)+[A-Za-z]{1,10})");
-
-        List<String> emails = new ArrayList<>();
-
-        Matcher m = p.matcher(text);
-        while (m.find()){
-            String email = m.group();
-            System.out.printf("Найденная почта - %s%n", email);
-            emails.add(email);
-        }
-        if (emails.size() == 0){
-            throw new NotFoundException("В фаиле нет почт");
-        }
-        return emails;
-    }
-
-    private List<String> getEmailsFromFile(MultipartFile file) throws FileReadException, NotFoundException{
-        try{
-            String content = new String(file.getBytes(), StandardCharsets.UTF_8);
-            System.out.println(content);
-            return findEmails(content);
-        } catch (IOException e){
-            throw new FileReadException("Ошибка открытия фаила");
-        } 
-    }
-
-    public void sendFileInvitations(InvitationDTO invitations) throws FileReadException{
-        sendInvitations(getEmailsFromFile(invitations.getFile()), invitations.getRoles());
     }
 
     public void sendInvitation(Invitation invitation) throws EmailSendException{
@@ -102,6 +59,9 @@ public class InvitationService {
         date.setTime(milliseconds);
         invitation.setDateExpired(date);
         invitation.setUrl(UUID.randomUUID().toString());
+        if (invitation.getRoles() == null){
+            throw new NotFoundException("Добавьте роли");
+        }
         try {
             sendEmail(
                 invitation.getEmail(), 
@@ -110,6 +70,8 @@ public class InvitationService {
             );
         } catch (MailSendException e) {
             throw new EmailSendException("Неправильный формат почты");
+        } catch (NullPointerException e) {
+            throw new NotFoundException("Добавьте почту");
         }
         invitationRepository.save(invitation);
     }
