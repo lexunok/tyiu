87eb3e10cba14515712228ded88de032b2d17c89
TimurMA <timur.minyazeff@gmail.com>
2023-08-08 01:17:57 +0500

    add deletingByEmail, existByEmail and findByEmail as optional method

--- src/main/java/com/tyiu/corn/repository/InvitationRepository.java
+++ src/main/java/com/tyiu/corn/repository/InvitationRepository.java
@@ -1,16 +1,22 @@
 package com.tyiu.corn.repository;
 
 import com.tyiu.corn.model.entities.Invitation;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
+import java.util.Optional;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Long>{
     @Modifying
     @Query("DELETE FROM Invitation i WHERE i.dateExpired < ?1")
     void deleteExpiredInvitations(Date date);
-    Invitation findByUrl(String url);
+    Optional<Invitation> findByUrl(String url);
+    boolean existsByEmail(String email);
+    @Modifying
+    @Query("DELETE FROM Invitation i WHERE i.email = ?1")
+    void deleteByEmail(String email);
 
 }
