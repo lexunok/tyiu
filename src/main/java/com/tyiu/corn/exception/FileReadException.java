11daf41a881f047ba5330dd1e201f3c44ccf9988
TimurMA <timur.minyazeff@gmail.com>
2023-08-08 01:25:41 +0500

    delete unused exception

--- src/main/java/com/tyiu/corn/exception/FileReadException.java
+++
@@ -1,9 +0,0 @@
-package com.tyiu.corn.exception;
-
-import java.io.IOException;
-
-public class FileReadException extends IOException{
-    public FileReadException(String message){
-        super(message);
-    }
-}
