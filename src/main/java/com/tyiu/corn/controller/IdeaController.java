df2a7d5ff87632bac1428428aab6ce396aca1603
wtckk <nikita.shutnikov89@gmail.com>
2023-08-08 15:15:26 +0500

    Controller update

--- src/main/java/com/tyiu/corn/controller/IdeaController.java
+++ src/main/java/com/tyiu/corn/controller/IdeaController.java
@@ -1,5 +1,7 @@
 package com.tyiu.corn.controller;
 
+import com.tyiu.corn.model.entities.Comment;
+import com.tyiu.corn.model.enums.StatusIdea;
 import org.springframework.web.bind.annotation.*;
 import com.tyiu.corn.service.IdeaService;
 import com.tyiu.corn.model.entities.Idea;
@@ -14,11 +16,6 @@ public class IdeaController {
     
     private final IdeaService ideaService;
 
-//    @GetMapping
-//    public List<Idea> showListIdea(){
-//        return ideaService.getListIdea();
-//    }
-
     @PostMapping("/add")
     public Idea addIdea(@RequestBody Idea idea) {
         return ideaService.saveIdea(idea);
@@ -29,8 +26,36 @@ public class IdeaController {
         ideaService.deleteIdea(id);
     }
 
-    @PutMapping("/admin/update/{id}")
-    public void updateIdeaByAdmin(@PathVariable Long id, @RequestBody Idea updatedIdea) {
-        ideaService.updateIdeaByAdmin(id, updatedIdea);
+    @PutMapping("/admin/update/{ideaId}")
+    public void updateIdeaByAdmin(@PathVariable Long ideaId, @RequestBody Idea updatedIdea) {
+        ideaService.updateIdeaByAdmin(ideaId, updatedIdea);
     }
+
+    @PutMapping("/admin/{ideaId}/add-comment")
+    public void createCommentAdmin(@PathVariable Long ideaId, @RequestBody Comment comment, @RequestBody String commentByAdmin){
+        ideaService.createComment(ideaId, comment, commentByAdmin);
+    }
+
+    @PutMapping("/project-office/update-status/{ideaId}")
+    public void updateStatusIdeaByProjectOffice(@PathVariable Long ideaId, @RequestBody StatusIdea newStatus){
+        ideaService.updateStatusByProjectOffice(ideaId, newStatus);
+    }
+
+    @PutMapping("/project-office/{ideaId}/add-comment")
+    public void createCommentProjectOffice(@PathVariable Long ideaId, @RequestBody Comment comment, @RequestBody String commentByProjectOffice){
+        ideaService.createComment(ideaId, comment, commentByProjectOffice);
+    }
+
+    @PutMapping("/expert/update-status/{ideaId}")
+    public void updateStatusByExpert(@PathVariable Long ideaId, @RequestBody StatusIdea newStatus){
+        ideaService.updateStatusByExpert(ideaId, newStatus);
+    }
+
+    @PutMapping("expert/add-risk/{ideaId}")
+    public void addRisk(@PathVariable Long ideaId, @RequestBody double riskValue){
+        ideaService.updateRiskByExpert(ideaId, riskValue);
+    }
+
+
+
 }
