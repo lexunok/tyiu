8f403e2cfe6f74976e5a9e6b30bfd2c43745b3dd
wtckk <nikita.shutnikov89@gmail.com>
2023-08-16 14:26:30 +0500

    Service chat

--- src/main/java/com/tyiu/corn/service/ChatService.java
+++ src/main/java/com/tyiu/corn/service/ChatService.java
@@ -5,8 +5,20 @@ import org.springframework.stereotype.Service;
 import com.tyiu.corn.repository.ChatRepository;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
 
     private final ChatRepository chatRepository;
-    
+
+    public void addMessageToChat(Long chatId, Message message) {
+        Chat chat = chatRepository.findById(chatId).orElseThrow(() -> new RuntimeException());
+        chat.getMessages().add(message);
+        chatRepository.save(chat);
+    }
+
+    public List<Message> getChatMessages(Long chatId) {
+        Chat chat = chatRepository.findById(chatId).orElseThrow(() -> new RuntimeException());
+        return chat.getMessages();
+    }
+
 }
