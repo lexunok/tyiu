09bf3e59300fbfce91ce464320fdb21f50e56b39
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-08-22 22:10:31 +0500

    Add files via upload

--- src/main/java/com/tyiu/corn/service/GroupService.java
+++ src/main/java/com/tyiu/corn/service/GroupService.java
@@ -2,19 +2,21 @@ package com.tyiu.corn.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.tyiu.corn.model.entities.Group;
 import com.tyiu.corn.repository.GroupRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class GroupService {
     
     @Autowired
     private final GroupRepository groupRepository;
 
     public Group getGroupById(Long id) {
-        Group group = groupRepository.findById(id).orElseThrow();
+        return groupRepository.findById(id).orElseThrow();
     }
     
     public Group createGroup(Group group) {
@@ -23,13 +25,16 @@ public class GroupService {
     }
     
     public Group updateGroup(Long id, Group group) {
-        Group group = groupRepository.findById(id).orElseThrow();
-        Group updatedGroup = groupRepository.save(group);
-    }
+    Group existingGroup = groupRepository.findById(id).orElseThrow();
+    return groupRepository.save(existingGroup);
+}
     
     public void deleteGroup(Long id) {
-        Group group = groupRepository.findById(id).orElseThrow();
-        groupRepository.delete(group);
-    }
+    Group group = groupRepository.findById(id).orElseThrow();
+    groupRepository.delete(group);
+}
       
 }
+
+
+
