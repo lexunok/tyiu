2bb31ad8604b49224cebe4fd40027a519b7991dd
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-08-23 12:56:59 +0500

    Add files via upload

--- src/main/java/com/tyiu/corn/repository/GroupRepository.java
+++ src/main/java/com/tyiu/corn/repository/GroupRepository.java
@@ -1,10 +1,13 @@
 package com.tyiu.corn.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.tyiu.corn.model.entities.Idea;
+import com.tyiu.corn.model.entities.Group;
 
-public interface GroupRepository extends JpaRepository<Idea, Long>{
-    
+public interface GroupRepository extends JpaRepository<Group, Long>{
+
+    List<Group> findAllById(Long id);
 
 }
