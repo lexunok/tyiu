6c7842ee37bcc17856e5baf3905f3c18e17e1429
lexunok <lex48949@gmail.com>
2023-08-24 23:10:08 +0500

    test action

--- src/main/java/com/tyiu/corn/config/WebSocketConfig.java
+++ src/main/java/com/tyiu/corn/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(MessageBrokerRegistry registry) {
-    registry.setApplicationDestinationPrefixes("/app");
+    registry.setApplicationDestinationPrefixes("/api");
     registry.enableSimpleBroker("/topic");
   }
 }
