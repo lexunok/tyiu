f2e8a80923a8636aabd83a9e8a81f29547cb6b13
lexunok <lex48949@gmail.com>
2023-08-25 17:18:59 +0500

    lettuce setup

--- src/main/java/com/tyiu/corn/config/RedisCacheConfig.java
+++ src/main/java/com/tyiu/corn/config/RedisCacheConfig.java
@@ -1,5 +1,6 @@
 package com.tyiu.corn.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -15,6 +18,20 @@ import java.time.Duration;
 @Configuration
 @EnableCaching
 public class RedisCacheConfig {
+
+    @Value("${spring.redis.host}")
+    String host;
+    @Value("${spring.redis.port}")
+    int port;
+    @Value("${spring.redis.password}")
+    String password;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host,port);
+        configuration.setPassword(password);
+        return new LettuceConnectionFactory(configuration);
+    }
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
