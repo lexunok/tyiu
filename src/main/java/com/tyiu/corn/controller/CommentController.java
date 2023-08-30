6667eba66cfbde6fa7ae9ccad8f5b522d7fc96bf
wtckk <nikita.shutnikov89@gmail.com>
2023-08-30 21:58:10 +0500

    update

--- src/main/java/com/tyiu/corn/controller/CommentController.java
+++ src/main/java/com/tyiu/corn/controller/CommentController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,23 +24,24 @@ import reactor.core.publisher.Mono;
 public class CommentController {
     private final CommentService commentService;
 
-//    @GetMapping("/get-idea-comments/{ideaId}")
-//    public Map<String, List<CommentDTO>> getAllIdeaComments(@PathVariable Long ideaId){
-//        return Map.of("comments", commentService.getAllIdeaComments(ideaId));
-//    }
+    @GetMapping("/get-idea-comments/{ideaId}")
+    public Map<String, Flux<CommentDTO>> getAllIdeaComments(@PathVariable Long ideaId){
+        return Map.of("comments", commentService.getAllIdeaComments(ideaId));
+    }
 //
+
 //    @MessageMapping("/comments/create-comment/{ideaId}")
 //    @SendTo("/topic/comments/{ideaId}")
 //    public Mono<CommentDTO> createComment(@DestinationVariable Long ideaId, @Payload CommentDTO commentDTO, Principal principal) {
 //        return commentService.createComment(ideaId, commentDTO, principal.getName());
 //    }
 
-    @MessageMapping("/comments/delete-comment/{commentId}")
-    @SendTo("/topic/comments/{ideaId}")
-    public Map<String, Long> deleteComment(@DestinationVariable Long commentId, Principal principal) {
-        commentService.deleteComment(commentId, principal.getName());
-        return Map.of("commentId", commentId);
-    }
+//    @MessageMapping("/comments/delete-comment/{commentId}")
+//    @SendTo("/topic/comments/{ideaId}")
+//    public Map<String, Long> deleteComment(@DestinationVariable Long commentId, Principal principal) {
+//        commentService.deleteComment(commentId, principal.getName());
+//        return Map.of("commentId", commentId);
+//    }
 
     @PostMapping("/add/{ideaId}")
     public Mono<CommentDTO> saveCommentOutdated(@RequestBody CommentDTO commentDTO, @PathVariable Long ideaId, Principal principal) {
