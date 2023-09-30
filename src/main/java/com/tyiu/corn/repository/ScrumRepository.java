938190cfca41628ac80cc80b44b9604cac095b11
Flakoooo <vlasov.vadom@gmail.com>
2023-09-30 18:10:55 +0500

    delete scrum and task

--- src/main/java/com/tyiu/corn/repository/ScrumRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tyiu.corn.repository;
-
-import com.tyiu.corn.model.entities.Scrum;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ScrumRepository extends ReactiveCrudRepository<Scrum, String> {
-}
--- src/main/java/com/tyiu/corn/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tyiu.corn.repository;
-
-import com.tyiu.corn.model.entities.Task;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends ReactiveCrudRepository<Task, String> {
-}
