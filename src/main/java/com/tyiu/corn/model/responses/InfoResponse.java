789a3cb57a1776028077e450d083c8225282f990
Flakoooo <vlasov.vadom@gmail.com>
2023-10-21 19:13:56 +0500

    Update InfoResponse.java

--- src/main/java/com/tyiu/corn/model/responses/InfoResponse.java
+++ src/main/java/com/tyiu/corn/model/responses/InfoResponse.java
@@ -1,7 +1,9 @@
 package com.tyiu.corn.model.responses;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
+@Data
 @AllArgsConstructor
 public class InfoResponse {
     private HttpStatus statusCode;
