e4ff1887ef767b223b665c8c0aef13b52bd2af0c
lexunok <lex48949@gmail.com>
2023-11-09 13:29:21 +0500

    fix stas

--- src/main/resources/db/migration/V7__create_team.sql
+++ src/main/resources/db/migration/V7__create_team.sql
@@ -1,13 +1,12 @@
-CREATE TABLE IF NOT EXISTS team_accessıon(
+CREATE TABLE IF NOT EXISTS team_accession (
     id BIGSERIAL PRIMARY KEY,
     team_id BIGINT REFERENCES team (id),
-    user_id BIGINT REFERENCES user (id),
+    user_id BIGINT REFERENCES users (id),
     modified_at TIMESTAMP,
     text TEXT,
     request_type TEXT NOT NULL,
     accession_stage TEXT NOT NULL,
     target_registered BOOLEAN NOT NULL,
-    target_email TEXT NOT NULL,
-    inviter_user_id BIGINT REFERENCES team_member (id)
+    target_email TEXT NOT NULL
 );
 DROP TABLE team_invitation;
