e66d983fd6a29421af28e88321f65f463db00c9a
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-11-11 15:00:41 +0300

    Add files via upload

--- src/main/java/com/tyiu/corn/model/entities/relations/Team2Member.java
+++ src/main/java/com/tyiu/corn/model/entities/relations/Team2Member.java
@@ -1,14 +1,15 @@
 package com.tyiu.corn.model.entities.relations;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "team_member")
 public class Team2Member {
-
     private Long teamId;
     private Long memberId;
 
