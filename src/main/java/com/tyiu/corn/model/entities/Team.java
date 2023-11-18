df4aeda1015cfcc2a5ce81094d32aac1fadb2e31
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-11-18 13:36:32 +0300

    Add files via upload

--- src/main/java/com/tyiu/corn/model/entities/Team.java
+++ src/main/java/com/tyiu/corn/model/entities/Team.java
@@ -1,10 +1,12 @@
 package com.tyiu.corn.model.entities;
 
+import com.tyiu.corn.model.dto.SkillDTO;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Setter
 @Getter
@@ -14,13 +16,14 @@ import java.time.LocalDate;
 @Table
 public class Team {
     @Id
-    private String id;
+    private Long id;
     private String name;
     private String description;
 
     private Boolean closed;
     private LocalDate createdAt;
 
-    private String ownerId;
-    private String leaderId;
+    private Long ownerId;
+    private Long leaderId;
+    private List<SkillDTO> desiredSkills;
 }
