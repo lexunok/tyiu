2e2e8944da8159604a8e8445631fbe594df695af
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-11-18 13:35:02 +0300

    Add files via upload

--- src/main/java/com/tyiu/corn/model/dto/TeamDTO.java
+++ src/main/java/com/tyiu/corn/model/dto/TeamDTO.java
@@ -11,14 +11,20 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeamDTO {
-    private String id;
+    private Long id;
+
     private String name;
     private String description;
+
     private Boolean closed;
     private Integer membersCount;
     private LocalDate createdAt;
+
     private TeamMemberDTO owner;
     private TeamMemberDTO leader;
+
     private List<TeamMemberDTO> members;
     private List<SkillDTO> skills;
+    private List<SkillDTO> desiredSkills;
+
 }
