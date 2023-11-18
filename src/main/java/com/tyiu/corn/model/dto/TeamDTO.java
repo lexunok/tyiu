a327ab71b08858aff523d2735f84ae37779ab9bd
HoseFisto <128784742+HoseFisto@users.noreply.github.com>
2023-11-18 16:11:33 +0300

    Add files via upload

--- src/main/java/com/tyiu/corn/model/dto/TeamDTO.java
+++ src/main/java/com/tyiu/corn/model/dto/TeamDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeamDTO {
-    private Long id;
+    private String id;
 
     private String name;
     private String description;
