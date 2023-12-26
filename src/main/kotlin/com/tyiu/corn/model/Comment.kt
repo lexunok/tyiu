e6e9faf525dc9d369866b3c02d00fb6211dd65d4
TimurMA <timur.minyazeff@gmail.com>
2023-12-26 21:42:03 +0500

    add sorting by created_at order

--- src/main/kotlin/com/tyiu/corn/model/Comment.kt
+++ src/main/kotlin/com/tyiu/corn/model/Comment.kt
@@ -4,12 +4,15 @@ import com.tyiu.corn.model.dto.UserDTO
 import com.tyiu.corn.model.entities.User
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.annotation.Id
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import java.time.LocalDateTime
 
 interface CommentRepository: CoroutineCrudRepository<Comment, String> {
-    fun findAllByIdeaId(ideaId: String): Flow<Comment>
+
+        @Query("SELECT * FROM comment WHERE idea_id = :ideaId ORDER BY created_at ASC")
+        fun findAllByIdeaId(ideaId: String): Flow<Comment>
 }
 interface UserRepository: CoroutineCrudRepository<User, String> {}
 
