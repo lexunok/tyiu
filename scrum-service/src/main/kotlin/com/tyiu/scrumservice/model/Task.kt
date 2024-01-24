677af5c31534c9518e31b6a4dffe6130d50a8e97
PetrovNikita a.k Shugizawa <silestrange@gmail.com>
2024-01-24 20:45:42 +0500

    add models

---
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/model/Task.kt
@@ -0,0 +1,78 @@
+package com.tyiu.scrumservice.model
+
+import com.tyiu.ideas.model.dto.UserDTO
+import com.tyiu.ideas.model.entities.User
+import jdk.javadoc.internal.tool.Start
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.time.LocalDate
+
+interface TaskRepository: CoroutineCrudRepository<Task, String>
+{
+}
+
+@Table
+data class Task (
+    @Id
+    val id: String? = null,
+    val sprintId: String? = null,
+    val projectId: String? = null,
+
+    val name: String? = null,
+    val description: String? = null,
+
+    val initiatorId: String? = null,
+    val executorId: String? = null,
+
+    val workHour: Long? = null,
+
+    val startDate: LocalDate? = null,
+    val finishDate: LocalDate? = null,
+
+    var status: String? = null
+)
+
+data class TaskDTO (
+    val id: String? = null,
+    val sprintId: String? = null, // Если можно сделать нулевое значение у ссылочного типа, иначе Task2Sprint
+    val projectId: String? = null,
+
+    val name:String? = null,
+    val description: String? = null,
+
+    val initiator: UserDTO? = null,
+    val executor: UserDTO? = null, //firstName lastName
+
+    val workHour: Long? = null,
+
+    val startDate: LocalDate? = null,
+    val finishDate: LocalDate? = null,
+
+    var tag: TaskTagDTO? = null,
+    var status: String? = null // = TaskStatus
+)
+//TaskStatus: InBackLog | OnModification | New | InProgress | OnVerification | Done
+
+fun Task.toDTO(): TaskDTO = TaskDTO (
+    id = id,
+    sprintId = sprintId,
+    projectId = projectId,
+
+    name = name,
+    description = description,
+
+    workHour = workHour,
+
+    startDate = startDate,
+    finishDate = finishDate,
+
+    status = status,
+)
+
+fun User.toDTO(): UserDTO = UserDTO (
+    id,
+    email,
+    firstName,
+    lastName,
+)
---
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/model/TaskMovementLog.kt
@@ -0,0 +1,7 @@
+package com.tyiu.scrumservice.model
+
+import jdk.javadoc.internal.tool.Start
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.time.LocalDate
