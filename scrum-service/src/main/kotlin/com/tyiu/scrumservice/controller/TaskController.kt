76c93ac2d99860ac34a4ac3a1c8f25f558e8b4c9
Shugizawa <Shug1zawa@yandex.ru>
2024-01-27 16:33:19 +0500

    scrum-service
    Исправил ошибки

--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/controller/TaskController.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/controller/TaskController.kt
@@ -5,10 +5,10 @@ import com.tyiu.scrumservice.service.TaskService
 import kotlinx.coroutines.flow.Flow
 import org.springframework.web.bind.annotation.*
 @RestController
-@RequestMapping("/api/v1/project")
-class TaskController (private val projectService: TaskService) {
+@RequestMapping("/api/v1/task")
+class TaskController (private val taskService: TaskService  ) {
 
     @GetMapping("/all")
-    fun getAllComments(): Flow<Task> = projectService.getAllProjects()
+    fun getAllComments(): Flow<Task> = taskService.getAllProjects()
 
 }
