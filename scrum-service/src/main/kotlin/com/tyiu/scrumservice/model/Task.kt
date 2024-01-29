80ca455a4abb58dff85f19a7fd4afdb2e0d7fe6f
Shugizawa <Shug1zawa@yandex.ru>
2024-01-30 02:42:29 +0500

    TASK api
    Add new reequest's in task api

--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/model/Task.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/model/Task.kt
@@ -8,12 +8,28 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import java.math.BigInteger
 import java.time.LocalDate
 
 interface TaskRepository: CoroutineCrudRepository<Task, String>
 {
-    @Query("SELECT * FROM task WHERE project_id = :projectId")
+    @Query("SELECT * FROM task WHERE project_id = :projectId ORDER BY start_date ASC") // ПОИСК ТАСКА ПО ПРОЕКТУ. СОРТИРОВКА ПО СОЗДАНИЮ ТАСКА
     fun findAllByProjectId(projectId: String): Flow<Task>
+    @Query(" SELECT * FROM task WHERE project_id = :projectId and status = 'InBacklog'") // ПРОСМОТР ТАСКОВ В БЭКЛОГЕ ПРОЕКТА
+    fun findAllInBacklog(projectId: String): Flow<Task>
+
+    @Query(" SELECT * FROM task WHERE project_id = :projectId and sprint_id = :sprintid ") // ПРОСМОТР ТАСКОВ В СПРИНТЕ ПРОЕКТА
+    fun findAllTaskBySprint(projectId: String, sprintId: String): Flow<Task>
+
+    @Query("SELECT * FROM task WHERE id = :id ") // ПОИСК ТАСКА ПО ЕГО АЙДИ
+    fun findTaskById(id: BigInteger): Flow<Task>
+
+    // УДАЛЕНИЕ ТАСКА ПО ЕГО АЙДИ. СДЕЛАЛ ОТДЕЛЬНЫЙ ЗАПРОС, ПОТОМУ ЧТО У НАС ID: STRING А В ТАБЛИЦЕ ID: BIGINTEGER. НЕ ПОНИМАЮ ПОЧЕМУ МЫ СДЕЛАЛИ ID = STRING
+    @Query("delete FROM task WHERE id = :id ")
+    fun deleteTaskById(id: BigInteger): Flow<Task>
+
+    /*@Query("SELECT * FROM task WHERE status = :status")  ПОИСК ТАСКА ПО ТЕГУ может пригодится в будущем
+    fun findAllInBacklog(status: String): Flow<Task>*/
 }
 
 @Table
@@ -29,7 +45,7 @@ data class Task (
     val initiatorId: String? = null,
     val executorId: String? = null,
 
-    val workHour: String? = null,
+    val workHour: Long? = null,
 
     val startDate: LocalDate? = LocalDate.now(),
     val finishDate: LocalDate? = LocalDate.now(),
@@ -53,7 +69,7 @@ data class TaskDTO (
     val initiator: UserDTO? = null,
     val executor: UserDTO? = null, //firstName lastName
 
-    val workHour: String? = null,
+    val workHour: Long? = null,
 
     val startDate: LocalDate? = LocalDate.now(),
     val finishDate: LocalDate? = LocalDate.now(),
@@ -64,7 +80,7 @@ data class TaskDTO (
 
 enum class TaskStatus
 {
-    InBackLog, OnModification, New, InProgress, OnVerification, Done
+    InBacklog, OnModification, New, InProgress, OnVerification, Done
 }
 
 
--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/service/TaskService.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/service/TaskService.kt
@@ -2,22 +2,47 @@ package com.tyiu.scrumservice.service
 
 import com.tyiu.ideas.model.CommentDTO
 import com.tyiu.ideas.model.toDTO
-import com.tyiu.scrumservice.model.Task
-import com.tyiu.scrumservice.model.TaskDTO
-import com.tyiu.scrumservice.model.TaskRepository
-import com.tyiu.scrumservice.model.toDTO
+import com.tyiu.scrumservice.model.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.springframework.stereotype.Service
+import java.math.BigInteger
 
 @Service
 class TaskService(private val repository: TaskRepository){
 
-    /*fun getAllTasks(projectId: String): Flow<TaskDTO> =
+    fun getAllTasksByProject(projectId: String): Flow<TaskDTO> =
         repository.findAllByProjectId(projectId).map {
             t -> val task = t.toDTO()
             return@map task
-        }*/
+        }
 
-    fun getAllProjects(): Flow<Task> = repository.findAll()
+    fun getAllTasksInBacklog(projectId: String): Flow<TaskDTO> =
+        repository.findAllInBacklog(projectId).map {
+            t -> val task = t.toDTO()
+            return@map task
+        }
+
+    fun getAllTasksInSprint(projectId: String, sprintId: String): Flow<TaskDTO> =
+        repository.findAllTaskBySprint(projectId, sprintId).map { t ->
+            val task = t.toDTO()
+            return@map task
+        }
+
+    fun getOneTaskById(id: BigInteger): Flow<TaskDTO> =
+        repository.findTaskById(id).map { t ->
+            val task = t.toDTO()
+            return@map task
+        }
+
+    fun getAllTasks(): Flow<Task> = repository.findAll() // ВЫВОД ВСЕХ СОЗДАННЫХ ТАСКОВ ВО ВСЕХ ПРОЕКТАХ. НУЖНО НА ВРЕМЯ РАЗРАБОТКИ
+
+    fun deleteTask(id: BigInteger) = repository.deleteTaskById(id)
+
+    /*fun getAllTasksInBacklog(status: String): Flow<TaskDTO> =  ПОИСК ТАСКА ПО ТЕГУ может пригодится в будущем
+    repository.findAllInBacklog(status).map {
+    t -> val task = t.toDTO()
+    return@map task
+    }*/
+
 }
