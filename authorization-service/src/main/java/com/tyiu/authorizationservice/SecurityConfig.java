a1ad70df48b3c890093b6da61186580b764e5c5f
lexunok <lex48949@gmail.com>
2024-02-18 21:29:44 +0500

    authorization decoder

--- authorization-service/src/main/java/com/tyiu/authorizationservice/SecurityConfig.java
+++ authorization-service/src/main/java/com/tyiu/authorizationservice/SecurityConfig.java
@@ -4,6 +4,8 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -38,6 +40,8 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/config/SecurityConfig.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/config/SecurityConfig.kt
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.server.SecurityWebFilterChain
+
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,5 +20,9 @@ class SecurityConfig {
             .csrf{it.disable()}
             .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
         return http.build()
+    }
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        return NimbusJwtDecoder.withIssuerLocation("http://127.0.0.1:7777").build()
     }
 }
