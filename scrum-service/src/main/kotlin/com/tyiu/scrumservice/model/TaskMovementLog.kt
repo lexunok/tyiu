33d065fdc9bfc3b86c292431e04ac8465508e640
Shugizawa <Shug1zawa@yandex.ru>
2024-02-19 18:47:56 +0500

    in working process with api and models in task

--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/model/TaskMovementLog.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/model/TaskMovementLog.kt
@@ -1,16 +1,16 @@
 package com.tyiu.scrumservice.model
 
+import com.tyiu.ideas.model.dto.UserDTO
 import kotlinx.coroutines.flow.Flow
 import org.springframework.data.annotation.Id
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import java.math.BigInteger
 import java.time.LocalDate
 
 interface TaskMovementLogRepository: CoroutineCrudRepository<TaskMovementLog, String>{
-    @Query("SELECT * FROM task_movement_log WHERE project_id = :projectId")
-    fun findLogByProjectId(projectId: String): Flow<TaskMovementLog>
+    @Query("SELECT * FROM task_movement_log WHERE task_id = :taskId")
+    fun findAllBytaskId(taskId: String): Flow<TaskMovementLog>
 }
 
 
@@ -22,7 +22,26 @@ data class TaskMovementLog (
     val taskId: String? = null,
     val executorId: String? = null,
     val userId: String? = null,
-    val editDate: LocalDate? = null,
-    val taskStatusA: TaskStatus? = null,
-    val taskStatusB: TaskStatus? = null,
+    val startDate: LocalDate? = LocalDate.now(),
+    val endDate: LocalDate? = null,
+    var status: TaskStatus? = null // предыдущий статус
+)
+
+data class TaskMovementLogDTO (
+    val id: String? = null,
+    val projectId: String? = null,
+    val task: TaskDTO? = null,
+    val executor: UserDTO? = null,
+    val user: UserDTO? = null,
+    val startDate: LocalDate? = LocalDate.now(),
+    val endDate: LocalDate? = null,
+    var status: TaskStatus? = null
+)
+
+fun TaskMovementLog.toDTO(): TaskMovementLogDTO = TaskMovementLogDTO (
+    id = id,
+    projectId = projectId,
+    startDate = startDate,
+    endDate = endDate,
+    status = status
 )
