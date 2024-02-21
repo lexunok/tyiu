508183ebe9d4d404d3ca9e3139b54096bfd612bb
lexunok <lex48949@gmail.com>
2024-02-21 14:00:05 +0500

    authorization config prod

--- authorization-service/src/main/java/com/tyiu/authorizationservice/SecurityConfig.java
+++ authorization-service/src/main/java/com/tyiu/authorizationservice/SecurityConfig.java
@@ -4,8 +4,6 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 import com.nimbusds.jose.jwk.JWKSet;
@@ -15,6 +13,7 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -40,8 +39,6 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
-import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -53,6 +50,12 @@ import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final UserRepository repository;
+    @Value("${oauth.secret}")
+    String secret;
+    @Value("${oauth.redirect}")
+    String redirect;
+    @Value("${oauth.issuer}")
+    String issuer;
     @Bean
     @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -78,9 +81,9 @@ public class SecurityConfig {
             throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/authorization/register").permitAll()
+                        .requestMatchers("/api/v1/authorization-service/register").permitAll()
                         .anyRequest().authenticated())
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/v1/authorization/register"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/v1/authorization-service/register"))
                 .formLogin(Customizer.withDefaults());
         return http.build();
     }
@@ -101,12 +104,11 @@ public class SecurityConfig {
     public RegisteredClientRepository registeredClientRepository(PasswordEncoder encoder) {
         RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId("gateway")
-                .clientSecret(encoder.encode("secret"))
+                .clientSecret(encoder.encode(secret))
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("http://localhost/login/oauth2/code/gateway")
+                .redirectUri(redirect)
                 .scope(OidcScopes.OPENID)
-                .scope("resource.read")
                 .clientSettings(ClientSettings.builder().requireAuthorizationConsent(false).build())
                 .build();
         return new InMemoryRegisteredClientRepository(oidcClient);
@@ -145,7 +147,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().issuer("http://127.0.0.1:7777").build();
+        return AuthorizationServerSettings.builder().issuer(issuer).build();
     }
     @Bean
     public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
