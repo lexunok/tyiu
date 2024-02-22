6c219ff9cfa35d5efd1f973f6dd53d5bf8c9678a
lexunok <lex48949@gmail.com>
2024-02-22 18:13:12 +0500

    scrum delete jwt decoder

--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/config/SecurityConfig.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/config/SecurityConfig.kt
@@ -1,21 +1,16 @@
 package com.tyiu.scrumservice.config
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.oauth2.jwt.JwtDecoder
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 
 
 @Configuration
 @EnableWebFluxSecurity
 class SecurityConfig {
-    @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    val issuer: String? = null
     @Bean
     fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
         http
@@ -23,9 +18,5 @@ class SecurityConfig {
             .csrf{it.disable()}
             .oauth2ResourceServer { it.jwt(Customizer.withDefaults()) }
         return http.build()
-    }
-    @Bean
-    fun jwtDecoder(): JwtDecoder {
-        return NimbusJwtDecoder.withIssuerLocation(issuer).build()
     }
 }
