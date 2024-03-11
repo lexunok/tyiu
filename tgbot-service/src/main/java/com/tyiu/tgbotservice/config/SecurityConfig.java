547b9cf9891fef5d42117d1ee7fe45b76c9b6823
kostet <kostagarifullin275@gmail.com>
2024-03-11 23:08:14 +0500

    Change user to jwt in telegram tag service

--- tgbot-service/src/main/java/com/tyiu/tgbotservice/config/SecurityConfig.java
+++ tgbot-service/src/main/java/com/tyiu/tgbotservice/config/SecurityConfig.java
@@ -1,22 +1,22 @@
 package com.tyiu.tgbotservice.config;
 
-import com.tyiu.tgbotservice.util.AuthenticationManager;
-import com.tyiu.tgbotservice.util.SecurityContextRepository;
+import reactor.core.publisher.Mono;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 
-import java.util.List;
+import java.util.Collection;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -24,48 +24,37 @@ import java.util.List;
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
 
-    private final AuthenticationManager authenticationManager;
-
-    private final SecurityContextRepository securityContextRepository;
-
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+
         http
+                .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .cors(Customizer.withDefaults())
-                .authorizeExchange(authorize -> authorize
-                        .pathMatchers("/api/v1/auth/**").permitAll()
-                        .pathMatchers("/api/v1/profile/get/invitation/**").permitAll()
-                        .pathMatchers("/api/v1/profile/change/password").permitAll()
-                        .pathMatchers("/api/v1/profile/delete/invitation/**").permitAll()
-                        .pathMatchers("/api/v1/profile/send/change/password").permitAll()
-                        .pathMatchers("/api/v1/profile/telegram/add-tag").permitAll()
-                        .pathMatchers("/api/**").authenticated()
-                        .anyExchange().permitAll())
-                .authenticationManager(authenticationManager)
-                .securityContextRepository(securityContextRepository);
+                .oauth2ResourceServer(resourceServer -> resourceServer
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(getJwtAuthenticationConverter())));
 
-                return http.build();
+        return http.build();
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    private Converter<Jwt, ? extends Mono<? extends AbstractAuthenticationToken>> getJwtAuthenticationConverter() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080","https://hits.tyuiu.ru","http://hits.tyuiu.ru"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**",configuration);
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(getJwtGrantedAuthoritiesConverter());
 
-        return source;
+        return jwt -> {
+            AbstractAuthenticationToken token = jwtAuthenticationConverter.convert(jwt);
+            return Mono.just(token);
+        };
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    private Converter<Jwt, Collection<GrantedAuthority>> getJwtGrantedAuthoritiesConverter() {
+
+        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
+        converter.setAuthorityPrefix("ROLE_");
+        converter.setAuthoritiesClaimName("roles");
+
+        return converter;
     }
 }
--- tgbot-service/src/test/java/com/tyiu/tgbotservice/telegram/TelegramTest.java
+++ tgbot-service/src/test/java/com/tyiu/tgbotservice/telegram/TelegramTest.java
@@ -1,58 +1,10 @@
 package com.tyiu.tgbotservice.telegram;
 
-import com.tyiu.tgbotservice.service.BotService;
 import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class TelegramTest extends TestContainersDB {
-
-    @Autowired
-    private BotService bot;
-
-    @Autowired
-    private  R2dbcEntityTemplate template;
-
-    private void createSQLTables() {
-
-        template.getDatabaseClient()
-                .sql("CREATE TABLE IF NOT EXISTS users " +
-                        "(id TEXT DEFAULT gen_random_uuid()::TEXT PRIMARY KEY, " +
-                        "email TEXT, " +
-                        "first_name TEXT, " +
-                        "last_name TEXT, " +
-                        "roles TEXT[], " +
-                        "password TEXT);")
-                .fetch()
-                .rowsUpdated()
-                .block();
-
-        template.getDatabaseClient()
-                .sql("CREATE TABLE IF NOT EXISTS users_telegram " +
-                        "(user_email TEXT REFERENCES users (email) ON UPDATE CASCADE, " +
-                        "user_tag TEXT, " +
-                        "chat_id BIGINT, " +
-                        "is_visible BOOLEAN DEFAULT false::BOOLEAN);")
-                .fetch()
-                .rowsUpdated()
-                .block();
-
-        template.getDatabaseClient()
-                .sql("CREATE TABLE IF NOT EXISTS notification_request " +
-                        "(id TEXT, " +
-                        "consumer_email TEXT REFERENCES users (email) ON UPDATE CASCADE, " +
-                        "consumer_tag TEXT REFERENCES users_telegram (user_tag) ON UPDATE CASCADE, " +
-                        "title TEXT, " +
-                        "message TEXT, " +
-                        "link TEXT, " +
-                        "button_name TEXT);")
-                .fetch()
-                .rowsUpdated()
-                .block();
-    }
-}
+public class TelegramTest extends TestContainersDB {}
