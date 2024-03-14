8e5b1029160491da0332707b283ba91d8300f1a7
SheludkovAlexei <144366359+SheludkovAlexei@users.noreply.github.com>
2024-03-14 21:27:53 +0500

    project and tag roles User and trycatch

--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/controller/ProjectController.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/controller/ProjectController.kt
@@ -1,80 +1,128 @@
 package com.tyiu.scrumservice.controller
 
 import com.tyiu.ideas.model.dto.IdeaMarketDTO
+import com.tyiu.ideas.model.entities.User
+import com.tyiu.ideas.model.enums.Role
 import com.tyiu.scrumservice.model.*
 import com.tyiu.scrumservice.service.ProjectService
-import kotlinx.coroutines.reactor.asFlux
+import kotlinx.coroutines.flow.Flow
 import org.springframework.http.HttpStatus
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 @RestController
 @RequestMapping("/api/v1/scrum-service/project")
 class ProjectController (private val projectService: ProjectService) {
 
     @GetMapping("/all")
-    @PreAuthorize("hasAnyRole('PROJECT_OFFICE' , 'ADMIN')")
-    fun getAllProjects(): Flux<ProjectDTO> = projectService.getAllProjects().asFlux()
+    fun getAllProjects(@AuthenticationPrincipal user: User): Flow<ProjectDTO>? {
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)){
+            projectService.getAllProjects()}
+        else {null}
+    }
 
     @GetMapping("/private/all")
-    @PreAuthorize("hasAnyRole('INITIATOR', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    fun getYourProjects(@AuthenticationPrincipal jwt: Jwt): Flux<ProjectDTO> = projectService.getYourProjects(jwt.id).asFlux()
+    fun getYourProjects(@AuthenticationPrincipal user: User): Flow<ProjectDTO>? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)) {
+            projectService.getYourProjects(user.id)}
+        else {null}
+    }
 
     @GetMapping("/active/all")
-    @PreAuthorize("hasAnyRole('INITIATOR', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    fun getYourActiveProjects(@AuthenticationPrincipal jwt: Jwt): Flux<ProjectDTO> = projectService.getYourActiveProjects(jwt.id).asFlux()
+    fun getYourActiveProjects(@AuthenticationPrincipal user: User): Flow<ProjectDTO>? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)) {
+            projectService.getYourActiveProjects(user.id)}
+        else {null}
+    }
 
     @GetMapping("/{projectId}")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    suspend fun getOneProject(@PathVariable projectId: String): Mono<ProjectDTO> = projectService.getOneProject(projectId).toMono()
+    suspend fun getOneProject(@PathVariable projectId: String,@AuthenticationPrincipal user: User): ProjectDTO? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            projectService.getOneProject(projectId)}
+        else {null}
+    }
 
     @GetMapping("/members/{projectId}/all")
-    @PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    fun getProjectMembers(@PathVariable projectId: String): Flux<ProjectMemberDTO>? = projectService.getProjectMembers(projectId)?.asFlux()
+    fun getProjectMembers(@PathVariable projectId: String,@AuthenticationPrincipal user: User): Flow<ProjectMemberDTO>? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            projectService.getProjectMembers(projectId)}
+        else {null}
+    }
 
     @GetMapping("/marks/{projectId}/all")
-    @PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    fun getProjectMarks(@PathVariable projectId: String): Flux<ProjectMarksDTO>? = projectService.getProjectMarks(projectId)?.asFlux()
+    fun getProjectMarks(@PathVariable projectId: String,@AuthenticationPrincipal user: User): Flow<ProjectMarksDTO>? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            projectService.getProjectMarks(projectId)}
+        else {null}
+    }
 
     @PostMapping("/send")
-    //@PreAuthorize("hasAnyRole('PROJECT_OFFICE' , 'ADMIN')")
-    suspend fun createProject(@RequestBody ideaMarketDTO: IdeaMarketDTO): Flux<ProjectMember> = projectService.createProject(ideaMarketDTO).asFlux()
+    suspend fun createProject(@RequestBody ideaMarketDTO: IdeaMarketDTO,@AuthenticationPrincipal user: User): Flow<ProjectMember>? {
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)){
+            projectService.createProject(ideaMarketDTO)}
+        else {null}
+    }
 
     @PostMapping("/{projectId}/add/members")
-    //@PreAuthorize("hasAnyRole('TEAM_OWNER', 'ADMIN')")
-    suspend fun addMembersInProject(@PathVariable projectId: String, @RequestBody teamMemberRequest: TeamMemberRequest): Mono<ProjectMember> = projectService.addMembersInProject(projectId,teamMemberRequest).toMono()
+    suspend fun addMembersInProject(@PathVariable projectId: String, @RequestBody teamMemberRequest: TeamMemberRequest,
+                                    @AuthenticationPrincipal user: User): ProjectMember? {
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.TEAM_OWNER)){
+            projectService.addMembersInProject(projectId,teamMemberRequest)}
+        else {null}
+    }
 
     @PostMapping("/{projectId}/add/marks")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'ADMIN')")
-    suspend fun addMarksInProject(@PathVariable projectId: String,@RequestBody projectMarksRequest: projectMarksRequest): Mono<ProjectMarks> = projectService.addMarksInProject(projectId, projectMarksRequest).toMono()
+    suspend fun addMarksInProject(@PathVariable projectId: String,@RequestBody projectMarksRequest: projectMarksRequest,
+                                  @AuthenticationPrincipal user: User): ProjectMarks?{
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE)){
+            projectService.addMarksInProject(projectId, projectMarksRequest)}
+        else {null}
+    }
 
     @PutMapping("/{projectId}/status/change")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'ADMIN')")
-    suspend fun pauseProject(@PathVariable projectId: String) : Mono<InfoResponse> {
-        return Mono.just(projectService.pauseProject(projectId))
-            .thenReturn(InfoResponse(HttpStatus.OK,"Статус проекта изменён"))
-            .onErrorReturn(InfoResponse(HttpStatus.BAD_REQUEST,"Статус проекта не был изменён"))
+    suspend fun pauseProject(@PathVariable projectId: String, @AuthenticationPrincipal user: User) : InfoResponse {
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE))
+        { try {
+            projectService.pauseProject(projectId)
+            InfoResponse(HttpStatus.OK, "Статус проекта изменён")
+        }
+        catch(e: Exception){
+            InfoResponse(HttpStatus.BAD_REQUEST, "Статус проекта не был изменён")
+        } }
+        else {InfoResponse(HttpStatus.BAD_REQUEST, "Статус проекта не был изменён")}
     }
 
     @PutMapping("/{projectId}/finish/change")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'ADMIN')")
-    suspend fun putFinishProject(@PathVariable projectId: String,@RequestBody projectFinishRequest: ProjectFinishRequest) : Mono<InfoResponse> {
-        return Mono.just(projectService.putFinishProject(projectId,projectFinishRequest))
-            .thenReturn(InfoResponse(HttpStatus.OK,"Проект успешно завершён"))
-            .onErrorReturn(InfoResponse(HttpStatus.BAD_REQUEST,"Проект не был завершён"))
+    suspend fun putFinishProject(@PathVariable projectId: String,@RequestBody projectFinishRequest: ProjectFinishRequest,
+                                 @AuthenticationPrincipal user: User) : InfoResponse {
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE))
+        { try {
+            projectService.putFinishProject(projectId,projectFinishRequest)
+            InfoResponse(HttpStatus.OK,"Проект успешно завершён")
+        }
+        catch(e: Exception){
+            InfoResponse(HttpStatus.BAD_REQUEST,"Проект не был завершён")
+        } }
+        else {InfoResponse(HttpStatus.BAD_REQUEST,"Проект не был завершён")}
     }
 
     @PutMapping("/leader/change")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'ADMIN')")
-    suspend fun putTeamLeader(@RequestBody projectLeaderRequest: ProjectLeaderRequest) : Mono<InfoResponse> {
-        return Mono.just(projectService.putTeamLeader(projectLeaderRequest))
-            .thenReturn(InfoResponse(HttpStatus.OK,"Лидер команды назначён"))
-            .onErrorReturn(InfoResponse(HttpStatus.BAD_REQUEST,"Лидер команды не был назначен"))
+    suspend fun putTeamLeader(@RequestBody projectLeaderRequest: ProjectLeaderRequest,@AuthenticationPrincipal user: User) : InfoResponse {
+        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE))
+        { try {
+            projectService.putTeamLeader(projectLeaderRequest)
+            InfoResponse(HttpStatus.OK,"Лидер команды назначён")
+        }
+        catch(e: Exception){
+            InfoResponse(HttpStatus.BAD_REQUEST,"Лидер команды не был назначен")
+        } }
+        else {InfoResponse(HttpStatus.BAD_REQUEST,"Лидер команды не был назначен")}
     }
 
 }
--- scrum-service/src/main/kotlin/com/tyiu/scrumservice/controller/TaskTagController.kt
+++ scrum-service/src/main/kotlin/com/tyiu/scrumservice/controller/TaskTagController.kt
@@ -1,43 +1,64 @@
 package com.tyiu.scrumservice.controller
 
 
+import com.tyiu.ideas.model.entities.User
+import com.tyiu.ideas.model.enums.Role
 import com.tyiu.scrumservice.model.InfoResponse
 import com.tyiu.scrumservice.model.TaskTagDTO
 import com.tyiu.scrumservice.model.TaskTagRequest
 import com.tyiu.scrumservice.service.TaskTagService
-import kotlinx.coroutines.reactor.asFlux
+import kotlinx.coroutines.flow.Flow
 import org.springframework.http.HttpStatus
-import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
 
 @RestController
 @RequestMapping("/api/v1/scrum-service/tag")
 class TaskTagController(private val tagService: TaskTagService) {
 
     @GetMapping("/{projectId}/all")
-    @PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    fun getAllTags(@PathVariable projectId: String): Flux<TaskTagDTO> = tagService.getAllTags(projectId).asFlux()
+    fun getAllTags(@PathVariable projectId: String,@AuthenticationPrincipal user: User): Flow<TaskTagDTO>? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            tagService.getAllTags(projectId)}
+        else {null}
+    }
 
     @PostMapping("/{projectId}/add")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    suspend fun createTag(@PathVariable projectId: String,@RequestBody taskTagDTO: TaskTagDTO): Mono<TaskTagDTO> = tagService.createTag(projectId, taskTagDTO).toMono()
+    suspend fun createTag(@PathVariable projectId: String,@RequestBody taskTagDTO: TaskTagDTO,
+                          @AuthenticationPrincipal user: User): TaskTagDTO? {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            tagService.createTag(projectId, taskTagDTO)}
+        else {null}
+    }
 
     @PutMapping("/{tagId}/update")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    suspend fun updateTag(@PathVariable tagId: String, @RequestBody taskTagRequest: TaskTagRequest) : Mono<InfoResponse> {
-        return Mono.just(tagService.updateTag(tagId,taskTagRequest))
-            .thenReturn(InfoResponse(HttpStatus.OK,"Тег был изменён успешно"))
-            .onErrorReturn(InfoResponse(HttpStatus.BAD_REQUEST,"Тег не был изменён"))
+    suspend fun updateTag(@PathVariable tagId: String, @RequestBody taskTagRequest: TaskTagRequest,
+                          @AuthenticationPrincipal user: User) : InfoResponse {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            try {
+                tagService.updateTag(tagId,taskTagRequest)
+                InfoResponse(HttpStatus.OK,"Тег был изменён успешно")
+            }
+            catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Тег не был изменён")
+            } }
+        else {InfoResponse(HttpStatus.BAD_REQUEST,"Тег не был изменён")}
     }
 
     @DeleteMapping("/{tagId}/delete")
-    //@PreAuthorize("hasAnyRole('INITIATOR', 'PROJECT_OFFICE', 'MEMBER', 'TEAM_OWNER', 'ADMIN')")
-    suspend fun deleteTag(@PathVariable tagId: String): Mono<InfoResponse> {
-        return Mono.just(tagService.deleteTag(tagId))
-            .thenReturn(InfoResponse(HttpStatus.OK,"Тег удалён успешно"))
-            .onErrorReturn(InfoResponse(HttpStatus.BAD_REQUEST,"Тег не был удалён"))
+    suspend fun deleteTag(@PathVariable tagId: String,@AuthenticationPrincipal user: User): InfoResponse {
+        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
+            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
+            try {
+                tagService.deleteTag(tagId)
+                InfoResponse(HttpStatus.OK,"Тег удалён успешно")
+            }
+            catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Тег не был удалён")
+            } }
+        else {InfoResponse(HttpStatus.BAD_REQUEST,"Тег не был удалён")}
     }
 }
