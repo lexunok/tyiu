924b432575c59ba2e24787a2e27b3002b89d00da
Flakoooo <vlasov.vadom@gmail.com>
2024-03-15 16:07:09 +0500

    user -> jwt

--- ideas-service/src/main/kotlin/com/tyiu/ideas/controller/ProjectController.kt
+++ ideas-service/src/main/kotlin/com/tyiu/ideas/controller/ProjectController.kt
@@ -2,125 +2,167 @@ package com.tyiu.ideas.controller
 
 import com.tyiu.ideas.model.*
 import com.tyiu.ideas.model.dto.IdeaMarketDTO
-import com.tyiu.ideas.model.entities.User
 import com.tyiu.ideas.model.enums.Role
 import com.tyiu.ideas.service.ProjectService
 import kotlinx.coroutines.flow.Flow
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/scrum-service/project")
 class ProjectController (private val projectService: ProjectService) {
 
+    private fun roleCheck(jwt: Jwt): Boolean {
+        return (jwt.getClaimAsStringList("roles").contains(Role.MEMBER.toString())
+                || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+                || jwt.getClaimAsStringList("roles").contains(Role.TEAM_OWNER.toString())
+                || jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+                || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+    }
+
     @GetMapping("/all")
-    fun getAllProjects(@AuthenticationPrincipal user: User): Flow<ProjectDTO>? {
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)){
-            projectService.getAllProjects()}
+    fun getAllProjects(@AuthenticationPrincipal jwt: Jwt): Flow<ProjectDTO>? {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+        {
+            projectService.getAllProjects()
+        }
         else {null}
     }
 
     @GetMapping("/private/all")
-    fun getYourProjects(@AuthenticationPrincipal user: User): Flow<ProjectDTO>? {
-        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
-            ||user.roles.contains(Role.ADMIN)) {
-            projectService.getYourProjects(user.id)}
+    fun getYourProjects(@AuthenticationPrincipal jwt: Jwt): Flow<ProjectDTO>? {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.MEMBER.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.TEAM_OWNER.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString()))
+        {
+            projectService.getYourProjects(jwt.id)
+        }
         else {null}
     }
 
     @GetMapping("/active/all")
-    fun getYourActiveProjects(@AuthenticationPrincipal user: User): Flow<ProjectDTO>? {
-        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
-            ||user.roles.contains(Role.ADMIN)) {
-            projectService.getYourActiveProjects(user.id)}
+    fun getYourActiveProjects(@AuthenticationPrincipal jwt: Jwt): Flow<ProjectDTO>? {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.MEMBER.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.TEAM_OWNER.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString()))
+        {
+            projectService.getYourActiveProjects(jwt.id)
+        }
         else {null}
     }
 
     @GetMapping("/{projectId}")
-    suspend fun getOneProject(@PathVariable projectId: String,@AuthenticationPrincipal user: User): ProjectDTO? {
-        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
-            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
-            projectService.getOneProject(projectId)}
+    suspend fun getOneProject(@PathVariable projectId: String,@AuthenticationPrincipal jwt: Jwt): ProjectDTO? {
+        return if (roleCheck(jwt))
+        {
+            projectService.getOneProject(projectId)
+        }
         else {null}
     }
 
     @GetMapping("/members/{projectId}/all")
-    fun getProjectMembers(@PathVariable projectId: String,@AuthenticationPrincipal user: User): Flow<ProjectMemberDTO>? {
-        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
-            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
-            projectService.getProjectMembers(projectId)}
+    fun getProjectMembers(@PathVariable projectId: String,@AuthenticationPrincipal jwt: Jwt): Flow<ProjectMemberDTO>? {
+        return if (roleCheck(jwt))
+        {
+            projectService.getProjectMembers(projectId)
+        }
         else {null}
     }
 
     @GetMapping("/marks/{projectId}/all")
-    fun getProjectMarks(@PathVariable projectId: String,@AuthenticationPrincipal user: User): Flow<ProjectMarksDTO>? {
-        return if (user.roles.contains(Role.MEMBER)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.TEAM_OWNER)
-            ||user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)) {
-            projectService.getProjectMarks(projectId)}
+    fun getProjectMarks(@PathVariable projectId: String,@AuthenticationPrincipal jwt: Jwt): Flow<ProjectMarksDTO>? {
+        return if (roleCheck(jwt))
+        {
+            projectService.getProjectMarks(projectId)
+        }
         else {null}
     }
 
     @PostMapping("/send")
-    suspend fun createProject(@RequestBody ideaMarketDTO: IdeaMarketDTO,@AuthenticationPrincipal user: User): Project? {
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.PROJECT_OFFICE)){
-            projectService.createProject(ideaMarketDTO)}
+    suspend fun createProject(@RequestBody ideaMarketDTO: IdeaMarketDTO,@AuthenticationPrincipal jwt: Jwt): Project? {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+        {
+            projectService.createProject(ideaMarketDTO)
+        }
         else {null}
     }
 
     @PostMapping("/{projectId}/add/members")
     suspend fun addMembersInProject(@PathVariable projectId: String, @RequestBody teamMemberRequest: TeamMemberRequest,
-                                    @AuthenticationPrincipal user: User): ProjectMember? {
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.TEAM_OWNER)){
-            projectService.addMembersInProject(projectId,teamMemberRequest)}
+                                    @AuthenticationPrincipal jwt: Jwt): ProjectMember? {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.TEAM_OWNER.toString()))
+        {
+            projectService.addMembersInProject(projectId,teamMemberRequest)
+        }
         else {null}
     }
 
     @PostMapping("/{projectId}/add/marks")
     suspend fun addMarksInProject(@PathVariable projectId: String,@RequestBody projectMarksRequest: projectMarksRequest,
-                                  @AuthenticationPrincipal user: User): ProjectMarks?{
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE)){
-            projectService.addMarksInProject(projectId, projectMarksRequest)}
+                                  @AuthenticationPrincipal jwt: Jwt): ProjectMarks?{
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+        {
+            projectService.addMarksInProject(projectId, projectMarksRequest)
+        }
         else {null}
     }
 
     @PutMapping("/{projectId}/status/change")
-    suspend fun pauseProject(@PathVariable projectId: String, @AuthenticationPrincipal user: User) : InfoResponse {
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE))
-        { try {
-            projectService.pauseProject(projectId)
-            InfoResponse(HttpStatus.OK, "Статус проекта изменён")
+    suspend fun pauseProject(@PathVariable projectId: String, @AuthenticationPrincipal jwt: Jwt) : InfoResponse {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+        {
+            try {
+                projectService.pauseProject(projectId)
+                InfoResponse(HttpStatus.OK, "Статус проекта изменён")
+            } catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST, "Статус проекта не был изменён")
+            }
         }
-        catch(e: Exception){
-            InfoResponse(HttpStatus.BAD_REQUEST, "Статус проекта не был изменён")
-        } }
         else {InfoResponse(HttpStatus.BAD_REQUEST, "Статус проекта не был изменён")}
     }
 
     @PutMapping("/{projectId}/finish/change")
     suspend fun putFinishProject(@PathVariable projectId: String,@RequestBody projectFinishRequest: ProjectFinishRequest,
-                                 @AuthenticationPrincipal user: User) : InfoResponse {
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE))
-        { try {
-            projectService.putFinishProject(projectId,projectFinishRequest)
-            InfoResponse(HttpStatus.OK,"Проект успешно завершён")
+                                 @AuthenticationPrincipal jwt: Jwt) : InfoResponse {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+        {
+            try {
+                projectService.putFinishProject(projectId,projectFinishRequest)
+                InfoResponse(HttpStatus.OK,"Проект успешно завершён")
+            } catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Проект не был завершён")
+            }
         }
-        catch(e: Exception){
-            InfoResponse(HttpStatus.BAD_REQUEST,"Проект не был завершён")
-        } }
         else {InfoResponse(HttpStatus.BAD_REQUEST,"Проект не был завершён")}
     }
 
     @PutMapping("/leader/change")
-    suspend fun putTeamLeader(@RequestBody projectLeaderRequest: ProjectLeaderRequest,@AuthenticationPrincipal user: User) : InfoResponse {
-        return if (user.roles.contains(Role.ADMIN)||user.roles.contains(Role.INITIATOR)||user.roles.contains(Role.PROJECT_OFFICE))
-        { try {
-            projectService.putTeamLeader(projectLeaderRequest)
-            InfoResponse(HttpStatus.OK,"Лидер команды назначен")
+    suspend fun putTeamLeader(@RequestBody projectLeaderRequest: ProjectLeaderRequest,@AuthenticationPrincipal jwt: Jwt) : InfoResponse {
+        return if (jwt.getClaimAsStringList("roles").contains(Role.ADMIN.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.INITIATOR.toString())
+            || jwt.getClaimAsStringList("roles").contains(Role.PROJECT_OFFICE.toString()))
+        {
+            try {
+                projectService.putTeamLeader(projectLeaderRequest)
+                InfoResponse(HttpStatus.OK,"Лидер команды назначен")
+            }
+            catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Лидер команды не был назначен")
+            }
         }
-        catch(e: Exception){
-            InfoResponse(HttpStatus.BAD_REQUEST,"Лидер команды не был назначен")
-        } }
         else {InfoResponse(HttpStatus.BAD_REQUEST,"Лидер команды не был назначен")}
     }
 
