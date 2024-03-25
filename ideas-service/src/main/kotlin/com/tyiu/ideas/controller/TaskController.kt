9f913f4e1c98c6fff45d8e21baf494970856883d
Flakoooo <vlasov.vadom@gmail.com>
2024-03-25 17:37:21 +0500

    add role check

--- ideas-service/src/main/kotlin/com/tyiu/ideas/controller/TaskController.kt
+++ ideas-service/src/main/kotlin/com/tyiu/ideas/controller/TaskController.kt
@@ -1,9 +1,13 @@
 package com.tyiu.ideas.controller
 
+import com.tyiu.ideas.config.exception.AccessException
 import com.tyiu.ideas.model.*
 import com.tyiu.ideas.model.entities.User
+import com.tyiu.ideas.model.enums.Role
 import com.tyiu.ideas.service.TaskService
+import com.tyiu.ideas.util.roleCheck
 import kotlinx.coroutines.flow.Flow
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
@@ -11,27 +15,107 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/api/v1/scrum-service/task")
 class TaskController (private val taskService: TaskService) {
 
+    private val roles = listOf(Role.INITIATOR, Role.PROJECT_OFFICE, Role.MEMBER, Role.TEAM_OWNER, Role.TEAM_LEADER, Role.ADMIN)
+
     @GetMapping("/project/all/{projectId}")
-    fun getAllTaskByProject(@PathVariable projectId: String): Flow<TaskDTO> = taskService.getAllTasksByProject(projectId)
+    fun getAllTaskByProject(@PathVariable projectId: String, @AuthenticationPrincipal user: User): Flow<TaskDTO> {
+        return if (user.roles.roleCheck(roles)) {
+            taskService.getAllTasksByProject(projectId)
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @GetMapping("/project/backlog/{projectId}")
-    fun getAllTasksInBackLog(@PathVariable projectId: String): Flow<TaskDTO> = taskService.getAllTasksInBacklog(projectId)
+    fun getAllTasksInBackLog(@PathVariable projectId: String, @AuthenticationPrincipal user: User): Flow<TaskDTO> {
+        return if (user.roles.roleCheck(roles)) {
+            taskService.getAllTasksInBacklog(projectId)
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @GetMapping("/project/sprint/{sprintId}")
-    fun getAllTasksInSprint(@PathVariable sprintId: String): Flow<TaskDTO> = taskService.getAllTasksInSprint(sprintId)
+    fun getAllTasksInSprint(@PathVariable sprintId: String, @AuthenticationPrincipal user: User): Flow<TaskDTO> {
+        return if (user.roles.roleCheck(roles)) {
+            taskService.getAllTasksInSprint(sprintId)
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @GetMapping("/{taskId}")
-    fun getOneTaskById(@PathVariable taskId: String): Flow<TaskDTO> = taskService.getOneTaskById(taskId)
+    fun getOneTaskById(@PathVariable taskId: String, @AuthenticationPrincipal user: User): Flow<TaskDTO> {
+        return if (user.roles.roleCheck(roles)) {
+            taskService.getOneTaskById(taskId)
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @PostMapping("/add")
-    suspend fun postCreateTask(@RequestBody taskDTO: TaskDTO, @AuthenticationPrincipal user: User): TaskDTO = taskService.createTask(taskDTO, user.id)
+    suspend fun postCreateTask(@RequestBody taskDTO: TaskDTO, @AuthenticationPrincipal user: User): TaskDTO {
+        return if (user.roles.roleCheck(roles)) {
+            taskService.createTask(taskDTO, user.id)
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @PutMapping("/update/{taskId}")
-    suspend fun putUpdateTask (@PathVariable taskId: String,@RequestBody taskDTO: TaskDTO) = taskService.putUpdateTask(taskId, taskDTO)
+    suspend fun putUpdateTask (@PathVariable taskId: String,
+                               @RequestBody taskDTO: TaskDTO,
+                               @AuthenticationPrincipal user: User): InfoResponse {
+        return if (user.roles.roleCheck(roles)) {
+            try {
+                taskService.putUpdateTask(taskId, taskDTO)
+                InfoResponse(HttpStatus.OK,"Задача успешно изменена")
+            }
+            catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Не удалось изменить задачу")
+            }
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @PutMapping("/executor/{taskId}/{executorId}")
-    suspend fun putUpdateExecutorTask(@PathVariable taskId: String, @PathVariable executorId: String) = taskService.putUpdateExecutorTask(taskId, executorId)
+    suspend fun putUpdateExecutorTask(@PathVariable taskId: String,
+                                      @PathVariable executorId: String,
+                                      @AuthenticationPrincipal user: User): InfoResponse {
+        return if (user.roles.roleCheck(roles)) {
+            try {
+                taskService.putUpdateExecutorTask(taskId, executorId)
+                InfoResponse(HttpStatus.OK,"Новый исполнитель успешно назначен")
+            }
+            catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Не удалось назначить исполнителя")
+            }
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 
     @DeleteMapping("/delete/{id}")
-    suspend fun deleteTask(@PathVariable id: String) = taskService.deleteTask(id)
+    suspend fun deleteTask(@PathVariable id: String, @AuthenticationPrincipal user: User): InfoResponse {
+        return if (user.roles.roleCheck(roles)) {
+            try {
+                taskService.deleteTask(id)
+                InfoResponse(HttpStatus.OK,"Задача успешно удалена")
+            }
+            catch(e: Exception){
+                InfoResponse(HttpStatus.BAD_REQUEST,"Ошибка при удалении задачи")
+            }
+        }
+        else {
+            throw AccessException("Нет прав")
+        }
+    }
 }
