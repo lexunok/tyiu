6e176c25d6c4a54404efc785f2150337c74339f3
lexunok <lex48949@gmail.com>
2024-04-26 21:17:06 +0500

    tg boy without mom

--- tgbot-service/src/main/java/com/tyiu/tgbotservice/config/RabbitMQConfig.java
+++ tgbot-service/src/main/java/com/tyiu/tgbotservice/config/RabbitMQConfig.java
@@ -1,48 +1,48 @@
 package com.tyiu.tgbotservice.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQConfig {
-
-    @Value("${rabbitmq.queue.receive.new}")
-    private String queueReceiveNew;
-
-    @Value("${rabbitmq.exchange}")
-    private String exchange;
-
-    @Value("${rabbitmq.routes.receive.new}")
-    private String routeReceivingNew;
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(exchange);
-    }
-
-    @Bean
-    public Binding binding() {
-        return BindingBuilder
-                .bind(new Queue(queueReceiveNew))
-                .to(exchange())
-                .with(routeReceivingNew);
-    }
-
-    @Bean
-    public MessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter());
-        return rabbitTemplate;
-    }
-}
+//import org.springframework.amqp.core.*;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.amqp.support.converter.MessageConverter;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQConfig {
+//
+//    @Value("${rabbitmq.queue.receive.new}")
+//    private String queueReceiveNew;
+//
+//    @Value("${rabbitmq.exchange}")
+//    private String exchange;
+//
+//    @Value("${rabbitmq.routes.receive.new}")
+//    private String routeReceivingNew;
+//
+//    @Bean
+//    public TopicExchange exchange() {
+//        return new TopicExchange(exchange);
+//    }
+//
+//    @Bean
+//    public Binding binding() {
+//        return BindingBuilder
+//                .bind(new Queue(queueReceiveNew))
+//                .to(exchange())
+//                .with(routeReceivingNew);
+//    }
+//
+//    @Bean
+//    public MessageConverter messageConverter() {
+//        return new Jackson2JsonMessageConverter();
+//    }
+//
+//    @Bean
+//    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.setMessageConverter(messageConverter());
+//        return rabbitTemplate;
+//    }
+//}
