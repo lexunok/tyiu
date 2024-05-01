771de92f7ed1422d95e4a406b918a121bcb3b783
Flakoooo <vlasov.vadom@gmail.com>
2024-05-01 16:29:27 +0500

    redis drink coffe

--- authorization-service/src/main/java/com/tyiu/authorizationservice/config/CacheConfig.java
+++ authorization-service/src/main/java/com/tyiu/authorizationservice/config/CacheConfig.java
@@ -1,27 +1,24 @@
 package com.tyiu.authorizationservice.config;
 
 
-import com.github.benmanes.caffeine.cache.Caffeine;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
-import java.util.concurrent.TimeUnit;
+import static java.time.Duration.ofMinutes;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
-
     @Bean
-    public CacheManager cacheManager() {
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCaffeine(Caffeine.newBuilder()
-                .maximumSize(100)
-                .expireAfterWrite(30, TimeUnit.MINUTES)
-                .recordStats());
-        return cacheManager;
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(ofMinutes(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
     }
 }
