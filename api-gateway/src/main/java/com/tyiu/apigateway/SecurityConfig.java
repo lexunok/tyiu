501e8edd03a4c80da6c21d773745ab8cac801fed
lexunok <lex48949@gmail.com>
2024-06-02 11:40:58 +0500

    cors v2

--- api-gateway/src/main/java/com/tyiu/apigateway/SecurityConfig.java
+++ api-gateway/src/main/java/com/tyiu/apigateway/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @EnableWebFluxSecurity
@@ -18,7 +23,18 @@ public class SecurityConfig {
                                 .pathMatchers("/auth/**", "/oauth2/**").permitAll()
                                 .anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost",
+                "https://hits.tyuiu.ru", "https://hits1.tyuiu.ru"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
