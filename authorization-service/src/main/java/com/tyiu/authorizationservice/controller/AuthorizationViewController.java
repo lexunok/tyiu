b4227863b447f6f7c83230f36e2c118e194effd9
lexunok <111588216+lexunok@users.noreply.github.com>
2024-06-19 10:18:56 +0500

    Update AuthorizationViewController.java

--- authorization-service/src/main/java/com/tyiu/authorizationservice/controller/AuthorizationViewController.java
+++ authorization-service/src/main/java/com/tyiu/authorizationservice/controller/AuthorizationViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/auth")
