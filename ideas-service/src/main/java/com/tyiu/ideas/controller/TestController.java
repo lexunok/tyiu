09f153742fac00f352e37bb726a8bd0d3b6705f9
Flakoooo <vlasov.vadom@gmail.com>
2024-08-23 18:46:10 +0500

    fix?

--- ideas-service/src/main/java/com/tyiu/ideas/controller/TestController.java
+++ ideas-service/src/main/java/com/tyiu/ideas/controller/TestController.java
@@ -59,7 +59,7 @@ public class TestController {
             return testService.generateFile(testName).flatMap(r -> {
                 HttpHeaders headers = new HttpHeaders();
                 headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + testName + ".txt");
-                headers.setContentType(MediaType.TEXT_PLAIN);
+                headers.setContentType(MediaType.valueOf(MediaType.TEXT_PLAIN_VALUE + "; charset=UTF-8"));
 
                 return Mono.just(new ResponseEntity<>(r, headers, HttpStatus.OK));
             });
