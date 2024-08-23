2dc36025b201322ae034085272bc084e2a742538
Flakoooo <vlasov.vadom@gmail.com>
2024-08-23 19:26:31 +0500

    мда

--- ideas-service/src/main/java/com/tyiu/ideas/controller/TestController.java
+++ ideas-service/src/main/java/com/tyiu/ideas/controller/TestController.java
@@ -58,7 +58,7 @@ public class TestController {
         try {
             return testService.generateFile(testName).flatMap(r -> {
                 HttpHeaders headers = new HttpHeaders();
-                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + testName + ".txt");
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + testName + ".xls");
                 headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_OCTET_STREAM + "; charset=UTF-16"));
 
                 return Mono.just(new ResponseEntity<>(r, headers, HttpStatus.OK));
